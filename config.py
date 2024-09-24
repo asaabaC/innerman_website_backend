52f1cd5ba993eb04fe6984d6fdfca6d982c53b9f
asaabaC <asaabacharlotte@gmail.com>
2024-09-24 18:10:46 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:@localhost/school"
+    JWT_SECRET_KEY='inner_man'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'inner_man'
